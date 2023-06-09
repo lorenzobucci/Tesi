8698d0f292e5cc258039ed12bfdf1a6ed12a1c54
Lorenzo Bucci <lorenzo.bucci@stud.unifi.it>
2023-06-09 12:33:36 +0200

    Implementati metodi di utilizzo servizi sul MobileDevice

--- src/mobiledevice/MobileDevice.java
+++ src/mobiledevice/MobileDevice.java
@@ -1,15 +1,47 @@
 package mobiledevice;
 
+import servicesmanagement.*;
+
+import java.util.HashSet;
+import java.util.Set;
 import java.util.UUID;
 
 public class MobileDevice {
 
-    private Trajectory pastTrajectory;
-    private UUID id;
+    public final UUID id = UUID.randomUUID();
+
+    private final Trajectory pastTrajectory = new Trajectory();
+
+    private final ServiceGateway serviceGateway = ServiceGateway.getInstance();
+
+    public Set<ServiceType> availableServiceTypes = serviceGateway.getAvailableServiceTypes(id);
+    public Set<WorkflowType> availableWorkflowTypes = serviceGateway.getAvailableWorkflowTypes(id);
+    public Set<ServiceInstance> ownedSingleServices = new HashSet<>();
+    public Set<WorkflowInstance> ownedWorkflows = new HashSet<>();
+
 
     public MobileDevice() {
-        id = UUID.randomUUID();
-        pastTrajectory = new Trajectory();
+
+    }
+
+    public void syncCurrentPosition(Position currentPosition) {
+        pastTrajectory.addPosition(currentPosition);
+    }
+
+    public void updateAvailableServiceTypes() {
+        availableServiceTypes = serviceGateway.getAvailableServiceTypes(id);
+    }
+
+    public void updateAvailableWorkflowTypes() {
+        availableWorkflowTypes = serviceGateway.getAvailableWorkflowTypes(id);
+    }
+
+    public void useService(ServiceType serviceType) {
+        ownedSingleServices.add(serviceGateway.instantiateService(serviceType.id));
+    }
+
+    public void useWorkflow(WorkflowType workflowType) {
+        ownedWorkflows.add(serviceGateway.instantiateWorkflow(workflowType.id));
     }
 
     public Trajectory getPastTrajectory() {
@@ -18,10 +50,6 @@ public class MobileDevice {
 
     public Position getCurrentPosition() {
         return pastTrajectory.getPositionsSet().last();
-    }
-
-    public void updateCurrentPosition(Position currentPosition) {
-        pastTrajectory.addPosition(currentPosition);
     }
 
 
