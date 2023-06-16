cbe426fd31c2d429f6839611c63daa71fcdcc2f5
Lorenzo Bucci <lorenzo.bucci@stud.unifi.it>
2023-06-16 18:48:34 +0200

    Rimosso stato da ServiceInstance

--- src/servicesmanagement/ServiceInstance.java
+++ src/servicesmanagement/ServiceInstance.java
@@ -3,8 +3,6 @@ package servicesmanagement;
 import mobiledevice.UserRequirements;
 
 import java.net.InetAddress;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.UUID;
 
 public class ServiceInstance {
@@ -12,24 +10,13 @@ public class ServiceInstance {
     public final UUID id = UUID.randomUUID();
 
     public final ServiceType serviceType;
-    private String serviceState = "IDLE";
     public final WorkflowInstance belongingWorkflow;
 
     public InetAddress nodeIpAddress;
-    private Map<String, Integer> internalState;
 
     ServiceInstance(ServiceType serviceType, WorkflowInstance belongingWorkflow) {
         this.serviceType = new ServiceType(serviceType);
         this.belongingWorkflow = belongingWorkflow;
-    }
-
-    public void syncWithRealObject(Map<String, Integer> internalState, String serviceState) {
-        this.internalState = new HashMap<>(internalState);
-        this.serviceState = serviceState;
-    }
-
-    public String getServiceState() {
-        return serviceState;
     }
 
     public ServiceRequirements getServiceRequirements() {
