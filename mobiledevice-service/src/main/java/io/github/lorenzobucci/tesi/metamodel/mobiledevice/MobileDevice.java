e3b145d6860d31e0577c82b4417b4050638615da
Lorenzo Bucci <lorenzo.bucci@stud.unifi.it>
2023-07-05 19:24:10 +0200

    Persistito il tipo di TrajectoryForecaster nel MobileDevice

--- mobiledevice-service/src/main/java/io/github/lorenzobucci/tesi/metamodel/mobiledevice/MobileDevice.java
+++ mobiledevice-service/src/main/java/io/github/lorenzobucci/tesi/metamodel/mobiledevice/MobileDevice.java
@@ -2,6 +2,7 @@ package io.github.lorenzobucci.tesi.metamodel.mobiledevice;
 
 import jakarta.persistence.*;
 
+import java.lang.reflect.InvocationTargetException;
 import java.net.URI;
 import java.util.UUID;
 
@@ -15,7 +16,7 @@ public class MobileDevice {
     @JoinColumn(name = "past_trajectory_id")
     private Trajectory pastTrajectory = new Trajectory();
 
-    @Transient // MAYBE NOT
+    @Transient // CLASS INSTANCE PERSISTED USING PROPERTY MODE
     private TrajectoryForecaster trajectoryForecaster;
 
     public MobileDevice() {
@@ -51,5 +52,18 @@ public class MobileDevice {
 
     public void setTrajectoryForecaster(TrajectoryForecaster trajectoryForecaster) {
         this.trajectoryForecaster = trajectoryForecaster;
+    }
+
+    @Access(AccessType.PROPERTY)
+    @Column(name = "trajectory_forecaster_class")
+    protected String getTrajectoryForecasterClass() {
+        if (trajectoryForecaster != null)
+            return trajectoryForecaster.getClass().getName();
+        return null;
+    }
+
+    protected void setTrajectoryForecasterClass(String trajectoryForecasterClass) throws ClassNotFoundException, NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
+        if (trajectoryForecasterClass != null)
+            trajectoryForecaster = (TrajectoryForecaster) Class.forName(trajectoryForecasterClass).getConstructor().newInstance();
     }
 }
