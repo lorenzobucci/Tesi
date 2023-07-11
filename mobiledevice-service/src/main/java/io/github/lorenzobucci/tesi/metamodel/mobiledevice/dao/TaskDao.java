868521a39424791b8be65cc70a6271291bf04bbf
Lorenzo Bucci <lorenzo.bucci@stud.unifi.it>
2023-07-11 17:53:56 +0200

    Implementato dao per i task

---
+++ mobiledevice-service/src/main/java/io/github/lorenzobucci/tesi/metamodel/mobiledevice/dao/TaskDao.java
@@ -0,0 +1,24 @@
+package io.github.lorenzobucci.tesi.metamodel.mobiledevice.dao;
+
+import io.github.lorenzobucci.tesi.metamodel.mobiledevice.model.Task;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.TypedQuery;
+
+import java.util.List;
+
+public class TaskDao {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    public List<Task> findAll() {
+        TypedQuery<Task> query = em.createQuery("from Task ", Task.class);
+        return query.getResultList();
+    }
+
+    public Task findById(long taskId) {
+        return em.find(Task.class, taskId);
+    }
+
+}
