f5a351578e4f82d995500981b626fe88870944dd
Lorenzo Bucci <lorenzo.bucci@stud.unifi.it>
2023-07-18 18:20:00 +0200

    Fix controller su TaskService

--- mobiledevice-service/src/main/java/io/github/lorenzobucci/tesi/metamodel/mobiledevice/service/gRPC/TaskService.java
+++ mobiledevice-service/src/main/java/io/github/lorenzobucci/tesi/metamodel/mobiledevice/service/gRPC/TaskService.java
@@ -1,7 +1,7 @@
 package io.github.lorenzobucci.tesi.metamodel.mobiledevice.service.gRPC;
 
 import com.google.protobuf.Empty;
-import io.github.lorenzobucci.tesi.metamodel.mobiledevice.dao.TaskDao;
+import io.github.lorenzobucci.tesi.metamodel.mobiledevice.controller.TaskController;
 import io.github.lorenzobucci.tesi.metamodel.mobiledevice.model.Task;
 import io.grpc.stub.StreamObserver;
 import jakarta.inject.Inject;
@@ -11,11 +11,11 @@ import java.util.List;
 public class TaskService extends TaskServiceGrpc.TaskServiceImplBase {
 
     @Inject
-    private TaskDao taskDao;
+    private TaskController taskController;
 
     @Override
     public void getTask(MobileDevice.TaskDTO request, StreamObserver<MobileDevice.TaskDTO> responseObserver) {
-        Task task = taskDao.findById(request.getId());
+        Task task = taskController.getTask(request.getId());
 
         MobileDevice.TaskDTO taskDTO = MobileDevice.TaskDTO.newBuilder().setId(task.getId())
                 .setEndpointURI(task.getEndpoint().toString())
@@ -28,7 +28,7 @@ public class TaskService extends TaskServiceGrpc.TaskServiceImplBase {
 
     @Override
     public void retrieveTasks(Empty request, StreamObserver<MobileDevice.TaskList> responseObserver) {
-        List<Task> tasks = taskDao.findAll();
+        List<Task> tasks = taskController.retrieveTasks();
         MobileDevice.TaskList.Builder taskList = MobileDevice.TaskList.newBuilder();
 
         for (Task task : tasks) {
