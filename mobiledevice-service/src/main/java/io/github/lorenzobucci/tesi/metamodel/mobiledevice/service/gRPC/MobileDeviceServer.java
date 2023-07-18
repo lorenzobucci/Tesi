01ef769ece96e47db23873b17669602d59ea9fba
Lorenzo Bucci <lorenzo.bucci@stud.unifi.it>
2023-07-18 15:15:09 +0200

    Implementato server gRPC per mobiledevice

---
+++ mobiledevice-service/src/main/java/io/github/lorenzobucci/tesi/metamodel/mobiledevice/service/gRPC/MobileDeviceServer.java
@@ -0,0 +1,51 @@
+package io.github.lorenzobucci.tesi.metamodel.mobiledevice.service.gRPC;
+
+
+import io.grpc.Grpc;
+import io.grpc.InsecureServerCredentials;
+import io.grpc.Server;
+
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+public class MobileDeviceServer {
+
+    private final Server server;
+
+    public MobileDeviceServer(int port) {
+        server = Grpc.newServerBuilderForPort(port, InsecureServerCredentials.create())
+                .addService(new MobileDeviceDTService())
+                .addService(new TaskService())
+                .build();
+    }
+
+    public static void main(String[] args) throws Exception {
+        MobileDeviceServer server = new MobileDeviceServer(8080);
+        server.start();
+        server.blockUntilShutdown();
+    }
+
+    public void start() throws IOException {
+        server.start();
+
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            try {
+                MobileDeviceServer.this.stop();
+            } catch (InterruptedException e) {
+                e.printStackTrace(System.err);
+            }
+        }));
+    }
+
+    public void stop() throws InterruptedException {
+        if (server != null) {
+            server.shutdown().awaitTermination(30, TimeUnit.SECONDS);
+        }
+    }
+
+    private void blockUntilShutdown() throws InterruptedException {
+        if (server != null) {
+            server.awaitTermination();
+        }
+    }
+}
