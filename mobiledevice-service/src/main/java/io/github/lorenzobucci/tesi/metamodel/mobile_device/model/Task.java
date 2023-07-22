76d9624cb826c90bb7e1bc5b83424c5bf3dd5294
Lorenzo Bucci <lorenzo.bucci@stud.unifi.it>
2023-07-22 17:15:52 +0200

    Implementato client per comunicazione tra mobile_device e services_management

--- mobiledevice-service/src/main/java/io/github/lorenzobucci/tesi/metamodel/mobile_device/model/Task.java
+++ mobiledevice-service/src/main/java/io/github/lorenzobucci/tesi/metamodel/mobile_device/model/Task.java
@@ -1,5 +1,11 @@
 package io.github.lorenzobucci.tesi.metamodel.mobile_device.model;
 
+import com.google.protobuf.Empty;
+import io.github.lorenzobucci.tesi.metamodel.services_management.service.gRPC.OperationalGrpc;
+import io.github.lorenzobucci.tesi.metamodel.services_management.service.gRPC.ServicesManagement;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import io.grpc.stub.StreamObserver;
 import jakarta.persistence.Column;
 import jakarta.persistence.Embedded;
 import jakarta.persistence.Entity;
@@ -25,7 +31,7 @@ public class Task extends BaseEntity {
         this.endpoint = endpoint;
         this.parameters = parameters;
         this.requirements = requirements;
-        //associatedWorkflowId = ServiceProxy.getInstance().requestService(this.toString()); // TODO: ADJUST AND USE API
+        associatedWorkflowId = ServicesManagementsClient.requestService(requirements, endpoint, parameters);
     }
 
     protected Task() {
@@ -34,11 +40,11 @@ public class Task extends BaseEntity {
 
     void updateRequirements(DependabilityRequirements requirements) {
         this.requirements = requirements;
-        //ServiceProxy.getInstance().updateServiceRequirements(associatedTaskId, this.toString()); // TODO: ADJUST USE API
+        ServicesManagementsClient.updateServiceRequirements(associatedWorkflowId, requirements);
     }
 
     void onCompleted() {
-        //ServiceProxy.getInstance().workflowcompleted; // TODO: ADJUST USE API
+        ServicesManagementsClient.workflowCompleted(associatedWorkflowId);
     }
 
     public URI getEndpoint() {
@@ -70,5 +76,92 @@ public class Task extends BaseEntity {
     @Override
     public int hashCode() {
         return endpoint.hashCode();
+    }
+
+    static class ServicesManagementsClient {
+
+        static ManagedChannel channel;
+        static OperationalGrpc.OperationalBlockingStub blockingStub;
+        static OperationalGrpc.OperationalStub asyncStub;
+
+        private static void init() {
+            channel = ManagedChannelBuilder.forAddress("localhost", 9001).usePlaintext().build();
+            blockingStub = OperationalGrpc.newBlockingStub(channel);
+            asyncStub = OperationalGrpc.newStub(channel);
+        }
+
+        private static long requestService(DependabilityRequirements requirements, URI endpoint, String parameters) {
+            init();
+
+            // DO CONVERSION FROM DependabilityRequirements TO WorkflowRequirementsDTO
+            ServicesManagement.WorkflowRequirementsDTO workflowRequirementsDTO = ServicesManagement.WorkflowRequirementsDTO.newBuilder().build();
+
+            ServicesManagement.WorkflowInstanceDTO workflowInstanceDTO = blockingStub.instantiateWorkflowInstance(ServicesManagement.InstantiateWorkflowParameters.newBuilder()
+                    .setEndpointParameters(parameters)
+                    .setEndpointURI(endpoint.toString())
+                    .setWorkflowRequirements(workflowRequirementsDTO)
+                    .build());
+
+            channel.shutdown();
+
+            return workflowInstanceDTO.getId();
+        }
+
+        private static void updateServiceRequirements(long associatedWorkflowId, DependabilityRequirements newRequirements) {
+            init();
+
+            // DO CONVERSION FROM DependabilityRequirements TO WorkflowRequirementsDTO
+            ServicesManagement.WorkflowRequirementsDTO workflowRequirementsDTO = ServicesManagement.WorkflowRequirementsDTO.newBuilder().build();
+
+            StreamObserver<Empty> streamObserver = new StreamObserver<>() {
+                @Override
+                public void onNext(Empty empty) {
+
+                }
+
+                @Override
+                public void onError(Throwable throwable) {
+                    // LOG FAILED UPDATE
+                }
+
+                @Override
+                public void onCompleted() {
+
+                }
+            };
+
+            asyncStub.updateWorkflowRequirements(ServicesManagement.UpdateRequirementsParameters.newBuilder()
+                    .setWorkflowInstance(ServicesManagement.WorkflowInstanceDTO.newBuilder().setId(associatedWorkflowId).build())
+                    .setNewWorkflowRequirements(workflowRequirementsDTO)
+                    .build(), streamObserver);
+
+            channel.shutdown();
+        }
+
+        private static void workflowCompleted(long associatedWorkflowId) {
+            init();
+
+            StreamObserver<Empty> streamObserver = new StreamObserver<>() {
+                @Override
+                public void onNext(Empty empty) {
+
+                }
+
+                @Override
+                public void onError(Throwable throwable) {
+                    // LOG FAILED TERMINATION
+                }
+
+                @Override
+                public void onCompleted() {
+
+                }
+            };
+
+            asyncStub.terminateWorkflowInstance(ServicesManagement.WorkflowInstanceDTO.newBuilder().setId(associatedWorkflowId).build(),
+                    streamObserver);
+
+            channel.shutdown();
+        }
     }
 }
