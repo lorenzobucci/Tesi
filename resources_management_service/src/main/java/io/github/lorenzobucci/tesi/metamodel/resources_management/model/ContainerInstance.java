ac81db5effaacc291de3b2b58909862d02c4c13e
Lorenzo Bucci <lorenzo.bucci@stud.unifi.it>
2023-07-25 18:19:22 +0200

    Rimosso pattern observer e semaforo migrazione

--- resources_management_service/src/main/java/io/github/lorenzobucci/tesi/metamodel/resources_management/model/ContainerInstance.java
+++ resources_management_service/src/main/java/io/github/lorenzobucci/tesi/metamodel/resources_management/model/ContainerInstance.java
@@ -2,30 +2,25 @@ package io.github.lorenzobucci.tesi.metamodel.resources_management.model;
 
 import jakarta.persistence.*;
 
-import java.beans.PropertyChangeListener;
-import java.beans.PropertyChangeSupport;
 import java.net.InetAddress;
-import java.util.concurrent.Semaphore;
 
 @Entity
 @Table(name = "container_instance")
 public class ContainerInstance extends BaseEntity {
 
-    @Transient
-    private final Semaphore migrationSemaphore = new Semaphore(1);
-    private final PropertyChangeSupport eventSupport = new PropertyChangeSupport(this);
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
     @JoinColumn(name = "container_type_id", nullable = false)
     private ContainerType containerType;
+
     @Column(name = "container_state", nullable = false)
     private String containerState = "IDLE";
+
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
     @JoinTable(name = "node_container_instances",
             joinColumns = {@JoinColumn(name = "container_instance_id", insertable = false,
                     updatable = false, referencedColumnName = "id")},
             inverseJoinColumns = {@JoinColumn(name = "node_id", insertable = false,
-                    updatable = false, referencedColumnName = "id")}
-    )
+                    updatable = false, referencedColumnName = "id")})
     private Node belongingNode;
 
     public ContainerInstance(ContainerType containerType, Node belongingNode) {
@@ -37,37 +32,16 @@ public class ContainerInstance extends BaseEntity {
 
     }
 
-    public void addPropertyChangeListener(PropertyChangeListener pcl) {
-        eventSupport.addPropertyChangeListener(pcl);
-    }
-
-    public void removePropertyChangeListener(PropertyChangeListener pcl) {
-        eventSupport.removePropertyChangeListener(pcl);
-    }
-
     public void syncWithRealObject(String containerState) {
-        if (migrationSemaphore.tryAcquire()) {
-            this.containerState = containerState; // NO NOTIFY
-            migrationSemaphore.release();
-        } else
-            throw new IllegalStateException("The container is being migrated, no changes possible.");
+        this.containerState = containerState;
     }
 
-    public void acquireMigrationSemaphore() throws InterruptedException {
-        migrationSemaphore.acquire();
-    }
-
-    public void releaseMigrationSemaphore() {
-        migrationSemaphore.release();
+    public void updateBelongingNode(Node newBelongingNode) {
+        belongingNode = newBelongingNode;
     }
 
     public String getContainerState() {
         return containerState;
-    }
-
-    public void setContainerState(String containerState) {
-        eventSupport.firePropertyChange("containerState", this.containerState, containerState);
-        this.containerState = containerState;
     }
 
     public ContainerType getContainerType() {
@@ -76,10 +50,6 @@ public class ContainerInstance extends BaseEntity {
 
     public Node getBelongingNode() {
         return belongingNode;
-    }
-
-    public void setBelongingNode(Node belongingNode) {
-        this.belongingNode = belongingNode;
     }
 
     public InetAddress getNodeIpAddress() {
--- resources_management_service/src/main/java/io/github/lorenzobucci/tesi/metamodel/resources_management/model/Node.java
+++ resources_management_service/src/main/java/io/github/lorenzobucci/tesi/metamodel/resources_management/model/Node.java
@@ -2,8 +2,6 @@ package io.github.lorenzobucci.tesi.metamodel.resources_management.model;
 
 import jakarta.persistence.*;
 
-import java.beans.PropertyChangeListener;
-import java.beans.PropertyChangeSupport;
 import java.net.InetAddress;
 import java.util.HashSet;
 import java.util.Set;
@@ -12,7 +10,12 @@ import java.util.Set;
 @Table(name = "node")
 public class Node extends BaseEntity {
 
-    private final PropertyChangeSupport eventSupport = new PropertyChangeSupport(this);
+    @OneToMany(cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+    @JoinTable(name = "node_container_instances",
+            joinColumns = {@JoinColumn(name = "node_id", referencedColumnName = "id")},
+            inverseJoinColumns = {@JoinColumn(name = "container_instance_id", referencedColumnName = "id")})
+    private Set<ContainerInstance> ownedContainers = new HashSet<>();
+
     @Enumerated(EnumType.STRING)
     @Column(name = "node_type", nullable = false)
     private NodeType nodeType;
@@ -31,12 +34,10 @@ public class Node extends BaseEntity {
     @Transient
     private float cpuUsagePercentage;
 
-    @OneToMany(cascade = {CascadeType.PERSIST, CascadeType.MERGE})
-    @JoinTable(name = "node_container_instances",
-            joinColumns = {@JoinColumn(name = "node_id", referencedColumnName = "id")},
-            inverseJoinColumns = {@JoinColumn(name = "container_instance_id", referencedColumnName = "id")}
-    )
-    private Set<ContainerInstance> ownedContainers = new HashSet<>();
+    public enum NodeType {
+        CLOUD,
+        EDGE
+    }
 
     public Node(NodeType nodeType, InetAddress ipAddress, NodeTechnicalProperties properties, float latitude, float longitude) {
         this.nodeType = nodeType;
@@ -49,29 +50,17 @@ public class Node extends BaseEntity {
     protected Node() {
     }
 
-    public void addPropertyChangeListener(PropertyChangeListener pcl) {
-        eventSupport.addPropertyChangeListener(pcl);
-    }
-
-    public void removePropertyChangeListener(PropertyChangeListener pcl) {
-        eventSupport.removePropertyChangeListener(pcl);
-    }
-
     public void syncWithRealObject(float memoryUsagePercentage, float cpuUsagePercentage) {
         this.memoryUsagePercentage = memoryUsagePercentage;
         this.cpuUsagePercentage = cpuUsagePercentage;
     }
 
     public void addOwnedContainer(ContainerInstance newContainer) {
-        Set<ContainerInstance> oldOwnedContainersCopy = new HashSet<>(ownedContainers);
         this.ownedContainers.add(newContainer);
-        eventSupport.firePropertyChange("ownedContainers", oldOwnedContainersCopy, ownedContainers);
     }
 
     public void removeOwnedContainer(ContainerInstance ownedContainer) {
-        Set<ContainerInstance> oldOwnedContainersCopy = new HashSet<>(ownedContainers);
         this.ownedContainers.remove(ownedContainer);
-        eventSupport.firePropertyChange("ownedContainers", oldOwnedContainersCopy, ownedContainers);
     }
 
     public NodeTechnicalProperties getProperties() {
@@ -104,11 +93,6 @@ public class Node extends BaseEntity {
 
     public int getOwnedContainersSize() {
         return ownedContainers.size();
-    }
-
-    public enum NodeType {
-        CLOUD,
-        EDGE
     }
 
 }
