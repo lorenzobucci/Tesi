4b43236ad14e9ac02c1eab5581e028879aa4e907
Lorenzo Bucci <lorenzo.bucci@stud.unifi.it>
2023-07-26 19:00:47 +0200

    Implementati controller per resources_management

---
+++ resources_management_service/src/main/java/io/github/lorenzobucci/tesi/metamodel/resources_management/controller/ContainerInstanceController.java
@@ -0,0 +1,46 @@
+package io.github.lorenzobucci.tesi.metamodel.resources_management.controller;
+
+import io.github.lorenzobucci.tesi.metamodel.resources_management.dao.ContainerInstanceDao;
+import io.github.lorenzobucci.tesi.metamodel.resources_management.model.ContainerInstance;
+import jakarta.inject.Inject;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+
+public class ContainerInstanceController {
+
+    @Inject
+    private ContainerInstanceDao containerInstanceDao;
+
+    @Inject
+    private ContainerTypeController containerTypeController;
+
+    @Inject
+    private NodeController nodeController;
+
+    @Inject
+    private AllocationController allocationController;
+
+    public ContainerInstance getContainerInstance(long containerInstanceId) throws NoSuchElementException {
+        ContainerInstance containerInstance = containerInstanceDao.findById(containerInstanceId);
+        if (containerInstance != null)
+            return containerInstance;
+        else
+            throw new NoSuchElementException("ContainerInstance with id=" + containerInstanceId + " does not exist.");
+    }
+
+    public List<ContainerInstance> retrieveContainerInstances() {
+        return containerInstanceDao.findAll();
+    }
+
+    public void destroyContainerInstance(long containerInstanceId) throws NoSuchElementException {
+        ContainerInstance containerInstance = getContainerInstance(containerInstanceId);
+        allocationController.deallocateContainer(containerInstance);
+        containerInstanceDao.delete(containerInstance);
+    }
+
+    ContainerInstance addContainerInstance(ContainerInstance containerInstance) {
+        containerInstanceDao.create(containerInstance);
+        return containerInstanceDao.findByUuid(containerInstance.getUuid());
+    }
+}
